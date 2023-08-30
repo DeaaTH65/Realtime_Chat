b89ce2ea5c946bfdf568c7915ef47de527a61985
Deaath65 <deaath65@gmail.com>
2023-08-30 13:01:05 +0545

    rooms

--- djangochat/djangochat/urls.py
+++ djangochat/djangochat/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('rooms/', include('room.urls')),
+    path('admin/', admin.site.urls),
 ]
